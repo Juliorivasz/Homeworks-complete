4760c10f890d67c8cd1f09a334b25752c6c54f7f
Juliorivasz <julioandresrivas@gmail.com>
2022-04-06 12:17:34 -0300

    complete

--- 05-EstructuraDeDatos-III/homework/homework.js
+++ 05-EstructuraDeDatos-III/homework/homework.js
@@ -8,8 +8,60 @@
 // corre depth-first (en recorrido "post-order" cuando depthFirstForEach() es ejecutado con la opcion "post-order"
 // corre breadth-first cuando breadthFirstForEach() es ejecutado
 // Observar imagen de la carpeta "homework" llamada "bst.png". Allí encontraran dibujado el arbol utilizado para los tests
-function BinarySearchTree() {
+function BinarySearchTree(value) {
+  this.value = value;
+  this.right = null;
+  this.left = null;
+}
 
+BinarySearchTree.prototype.contains = function(value){
+  if(value === this.value)return true;
+  if(value > this.value){
+    if(!this.right)return false;
+    else return this.right.contains(value);
+  }else{
+    if(!this.left)return false;
+    else return this.left.contains(value);
+  }
+}
+
+BinarySearchTree.prototype.size = function(){
+  if(!this.right && !this.left)return 1;
+  if(!this.left) return 1 + this.right.size();
+  if(!this.right) return 1 +this.left.size();
+  return 1 + this.left.size() + this.right.size();
+}
+BinarySearchTree.prototype.insert = function(value){
+  if(value === this.value)return undefined;
+  if(value > this.value){
+    if(!this.right){
+      this.right = new BinarySearchTree(value)
+    }else {
+      this.right.insert(value);
+    }
+  } else if(value < this.value){
+    if(!this.left){
+      this.left = new BinarySearchTree(value)
+    }else {
+      this.left.insert(value);
+    }
+  }
+
+}
+
+BinarySearchTree.prototype.depthFirstForEach = function(cb, order){
+  if(order === "pre-order") cb(this.value);
+  if(this.left) this.left.depthFirstForEach(cb,order);
+  if(!order || order === "in-order") cb(this.value);
+  if(this.right)this.right.depthFirstForEach(cb,order);
+  if(order === "post-order") cb(this.value);
+}
+
+BinarySearchTree.prototype.breadthFirstForEach = function(cb,list=[]){
+  cb(this.value)
+  if(this.left)list.push(this.left)
+  if(this.right)list.push(this.right)
+  list.length && list.shift().breadthFirstForEach(cb,list)
 }
 
 // No modifiquen nada debajo de esta linea
--- 06-Algoritmos-I/homework/homework.js
+++ 06-Algoritmos-I/homework/homework.js
@@ -6,24 +6,53 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
-}
-
+  var arr = [1]
+  var dividir = 2
+  while( num > 1){
+    if(num % dividir === 0){
+      arr.push(dividir)
+      num = num / dividir;
+    }else{
+      if(dividir % 2 === 0)dividir += 1
+      else dividir += 2;
+    }
+  }
+  return arr;
+}          
 function bubbleSort(array) {
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  let swap = true;
+  while(swap){
+    swap = false
+  for(let i = 0; i < array.length - 1; i++){
+    if(array[i] > array[i+1]){
+      let value = array[i]
+      array[i] = array[i+1]
+      array[i+1] = value
+      swap = true
+    }
+  }
 }
-
-
+  return array;
+}        
 function insertionSort(array) {
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+for(let i = 1; i < array.length; i++){
+  let p = i-1;
+  let value = array[i];
+  while(p >= 0 && value < array[p]){
+    array[p+1] = array[p];
+    p--;
+  }
+  array[p+1] = value;
+ } 
+  return array;
 }
 
 
@@ -32,9 +61,21 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for(let i= 0; i<array.length;i++){
+    let min = i;
+    for(let j= i+1; j < array.length; j++){
+      if(array[min] > array[j]){
+        min = j
+      }
+    }
+    if(min !== i){
+      let select = array[i]
+      array[i] = array[min]
+      array[min] = select
+    }
 }
-
+return array;
+}
 
 // No modificar nada debajo de esta línea
 // --------------------------------
