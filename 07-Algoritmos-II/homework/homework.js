55adabbd5ddc35e46e816e65a82a2f38c6423388
Juliorivasz <julioandresrivas@gmail.com>
2022-04-07 19:44:03 -0300

    algoritmos-II solucion

--- 07-Algoritmos-II/homework/homework.js
+++ 07-Algoritmos-II/homework/homework.js
@@ -6,15 +6,47 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  let pivot = array[Math.floor(array.length/2)];
+  let letf = [];
+  let rigth = [];
+  let centro = []
+  if(array.length <= 1) return array;
+  for(let i = 0; i<array.length;i++){
+    if(array[i] < pivot){
+      letf.push(array[i])
+    }else if(array[i] > pivot){
+      rigth.push(array[i])
+    }else centro.push(array[i])
+  }
+   return quickSort(letf).concat(centro).concat(quickSort(rigth));
+  }
 
-}
-
+  function merge(letf,rigth){
+    let result = [];
+    let indexleft = 0;
+    let indexrigth = 0;
+    while(indexleft < letf.length && indexrigth < rigth.length){
+      if(letf[indexleft]<rigth[indexrigth]){
+        result.push(letf[indexleft])
+        indexleft++;
+      }else{
+        result.push(rigth[indexrigth])
+        indexrigth++;
+      } 
+    }
+    return result.concat(letf.slice(indexleft),rigth.slice(indexrigth));
+  }
+    
 function mergeSort(array) {
   // Implementar el método conocido como mergeSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
-  // Tu código:
-
+  // Tu código: 
+  let mid = Math.floor(array.length/2);
+  let letf = array.slice(0, mid);
+  let rigth = array.slice(mid);
+  if(array.length === 1) return array;
+  return merge(mergeSort(letf),mergeSort(rigth));
 }
 
 // No modificar nada debajo de esta línea
